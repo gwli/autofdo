00ec498d76bfed82d1956b4adbc89412f8fc4593
victor <vili@nvidia.com>
2019-08-12 19:25:25 +0800

    add build scripts

---
+++ victor/build.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get -y install autoconf automake git libelf-dev libssl-dev pkg-config clang-5.0 libtool
+cd ..
+aclocal -I .; autoheader; autoconf; automake --add-missing -c
+CC=clang-5.0 CXX=clang++-5.0 ./configure  --with-llvm=$(which llvm-config-5.0)
+make 
+
