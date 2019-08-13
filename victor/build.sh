3cd56dd481046432d21f4b377bde04577dc03246
victor <vili@nvidia.com>
2019-08-13 11:41:05 +0800

    add basic sample

--- victor/build.sh
+++ victor/build.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get -y install autoconf automake git libelf-dev libssl-dev pkg-config clang-5.0 libtool
+apt-get -y install autoconf automake git libelf-dev libssl-dev pkg-config clang-5.0 libtool linux-tools-$(uname -r)
 cd ..
 aclocal -I .; autoheader; autoconf; automake --add-missing -c
 CC=clang-5.0 CXX=clang++-5.0 ./configure  --with-llvm=$(which llvm-config-5.0)
